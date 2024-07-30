5c46db0836d95d8bc57969588e868bdde9718921
admin <piccolisantiagonicolas@gmail.com>
2024-07-30 14:07:58 -0300

    -fixed a typo. This short project is now successfully concluded. 🚀

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const toFahrenheit = document.getElementById("toFahrenheit");
 const toCelsius = document.getElementById("toCelsius");
 const resultTemp = document.getElementById("resultTemp");
 
-function convert(){
-    const temp = parseFloat(textBox.value); // Convert input value to a floating-point number
 
+function convert() {
+    const temp = parseFloat(textBox.value); // Convert input value to a floating-point number
     if (toFahrenheit.checked) {
-        resultTemp.textContent = (temp * 1.8 + 32).toFixed(2); // Convert to Fahrenheit
+        resultTemp.textContent = (temp * 1.8 + 32).toFixed(2)+ "°F"; // Convert to Fahrenheit
     } else if (toCelsius.checked) {
-        resultTemp.textContent = ((temp - 32) / 1.8).toFixed(2); // Convert to Celsius
+        resultTemp.textContent = ((temp - 32) / 1.8).toFixed(2) + "°C"; // Convert to Celsius
     } else {
         resultTemp.textContent = "Select a unit";
     }
